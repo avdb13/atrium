d05b6c093184ce555b80ad6d72f90be1db753200
avdb13 <avdb@keemail.me>
2024-11-09 05:26:48 +0000

    introduce `SessionStore`

--- atrium-oauth/oauth-client/src/store.rs
+++ atrium-oauth/oauth-client/src/store.rs
@@ -1,6 +1,9 @@
+use atrium_api::types::string::Did;
 use atrium_common::store::{memory::MemoryMapStore, MapStore};
 use jose_jwk::Key;
 use serde::{Deserialize, Serialize};
+
+use crate::TokenSet;
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct InternalStateData {
@@ -14,3 +17,15 @@ pub trait StateStore: MapStore<String, InternalStateData> {}
 pub type MemoryStateStore = MemoryMapStore<String, InternalStateData>;
 
 impl StateStore for MemoryStateStore {}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct Session {
+    pub dpop_key: Key,
+    pub token_set: TokenSet,
+}
+
+pub trait SessionStore: MapStore<Did, Session> {}
+
+pub type MemorySessionStore = MemoryMapStore<Did, Session>;
+
+impl SessionStore for MemorySessionStore {}
